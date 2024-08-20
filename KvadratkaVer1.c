60be82d218eec5c962ab8871664401e6b958bbc5
Gugku <kirlaz07@yandex.ru>
2024-08-20 20:03:38 +0300

    ver. 1.5
    Небольшие изменения

--- KvadratkaVer1.c
+++ KvadratkaVer1.c
@@ -2,79 +2,34 @@
 #include <TXLib.h>
 #include <math.h>
 
-const int Lyboe = -1;                                                                              /*Глобальная переменная*/
+const int ALLNUMBERS = -1;                                                                  /*Глобальная переменная*/
 
-    /*-----------------------Модуль вычисления(функция, которая будет находить корни)---- */
-int ReshenieKvadratki( double a, double b, double c, double* x1, double* x2)
-        {
-
-            if (a == 0)
-                {
-                    if (b == 0)
-                        {
-                            return (c == 0)? Lyboe : 0;
-                        }
-                    else
-                        {
-                            *x1 = -c / b;
-                            return 1;
-                        }
-                }
-            else
-                {
-                    if (b == 0)
-                        {
-                            *x1 =   sqrt(c/a);
-                            *x2 = - sqrt(c/a);
-                            return 2;
-                        }
-                    else
-                        {
-                            double discriminant = b*b - 4*a*c;
-
-                            if (discriminant < 0)
-                                {
-                                    return 0;
-                                }
-                            if (discriminant == 0)
-                                {
-                                    *x1 = (- b + sqrt(discriminant))/(2*a);
-                                    return 1;
-                                }
-                            if (discriminant > 0)
-                                {
-                                    *x1 = (- b + sqrt(discriminant))/(2*a);
-                                    *x2 = (- b - sqrt(discriminant))/(2*a);
-                                    return 2;
-                                }
-                        }
-                }
-
-
-        }
-    /*------------------------------------------------------------------------------------*/
-
+int SolutionOfQuadratic( double a, double b, double c, double* x1, double* x2);             /*Обьявление функции, решающей квадратное уравнение*/
 
 int main(void)
     {
     /*-----------------------Модуль ввода данных------------------------------------------*/
-        printf  ("#Реши квадратное уравнение.\n");                                          /*Описание нашей программы*/
-        printf  ("#Введите коэффиценты через запятую с дробной частью a,b,c:");             /*Обращение к пользователю*/
-        double a=0, b=0, c=0;                                                               /*Вводим три переменных, которые являются коэффицентами квадратного уравнения*/
-        scanf   ("%lg,%lg,%lg", &a, &b, &c);                                                /*Считываем, что вводит пользователь*/
-        printf  ("Вы ввели: %f, %f, %f\n", a, b, c);                                          /*Выводим числа введеные пользователем*/
+        double a = 0, b=0, c=0;                                                             /*Вводим три переменных, которые являются коэффицентами квадратного уравнения*/
+        int SuccessfulInput = 0;
+        while (SuccessfulInput == 0)                                                        /*Считываем, что вводит пользователь*/
+            {
+            printf  ("#Реши квадратное уравнение.\n");                                      /*Описание нашей программы*/
+            printf  ("#Введите коэффиценты через запятую с дробной частью a,b,c:");         /*Обращение к пользователю*/
+            SuccessfulInput = scanf   ("%lg,%lg,%lg", &a, &b, &c);
+            }
+        printf  ("Вы ввели: %f, %f, %f\n", a, b, c);                                        /*Выводим числа введеные пользователем*/
         double x1=0, x2=0;                                                                  /*Вводим переменные, в которые будут записываться корни*/
     /*------------------------------------------------------------------------------------*/
 
 
     /*-----------------------Модуль запроса на вывод результата---------------------------*/
-        int VozvratDlyaSwitch = ReshenieKvadratki( a, b, c, &x1, &x2);
+        int SwitchReturn = SolutionOfQuadratic( a, b, c, &x1, &x2);
     /*------------------------------------------------------------------------------------*/
 
 
 
     /*-----------------------Модуль вывода результата-------------------------------------*/
-        switch (VozvratDlyaSwitch)
+        switch (SwitchReturn)
         {
             case 0: printf("Нет решений\n");
                     break;
@@ -85,12 +40,60 @@ int main(void)
             case 2: printf("x1 = %lg, x2 = %lg\n", x1, x2);
                     break;
 
-            case Lyboe: printf("Решением является любое число");
+            case ALLNUMBERS: printf("Решением является любое число");
                         break;
 
-            default: printf ("main(): ERROR: VozvratDlyaSwitch = %d\n", VozvratDlyaSwitch);
+            default: printf ("main(): ERROR: SwitchReturn = %d\n", SwitchReturn);
                      return 1;
         }
     /*------------------------------------------------------------------------------------*/
 
     }
+
+    /*-----------------------Модуль вычисления(функция, которая будет находить корни)---- */
+int SolutionOfQuadratic( double a, double b, double c, double* x1, double* x2)
+    {
+
+    if (a == 0)
+        {
+        if (b == 0)
+            {
+            return (c == 0)? ALLNUMBERS : 0;
+            }
+        else
+            {
+            *x1 = -c / b;
+            return 1;
+            }
+    }
+    else
+        {
+        if (b == 0)
+            {
+            *x1 =   sqrt(c/a);
+            *x2 = - sqrt(c/a);
+            return 2;
+            }
+        else
+            {
+            double discriminant = b*b - 4*a*c;
+
+            if (discriminant < 0)
+                {
+                return 0;
+                            }
+            if (discriminant == 0)
+                {
+                *x1 = (- b + sqrt(discriminant))/(2*a);
+                return 1;
+                }
+            if (discriminant > 0)
+                {
+                *x1 = (- b + sqrt(discriminant))/(2*a);
+                *x2 = (- b - sqrt(discriminant))/(2*a);
+                return 2;
+                }
+            }
+        }
+    }
+    /*------------------------------------------------------------------------------------*/
